2504db353f8a10362a91d530f3306faaef638937
Leonard <>
2023-07-06 12:23:09 +0200

    starting development

---
+++ common/constants.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#define DEFAULT_PORT 61970
+
+// position of data in the header as an array
+#define POS_VERSION_MAJOR 0
+#define POS_VERSION_MINOR 1
+#define POS_METHOD        2
+#define POS_VARIANT       3
+#define POS_TIME          6
+#define POS_UUID          32
+#define POS_DATA_START    64
+
+// Method values meaning
+#define METHOD_GET   0
+#define METHOD_POST  1
+#define VARIANT_TEXT 0
+#define VARIANT_BIN  1
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,16 +1,9 @@
 #include "tcpConn.hpp"
 
+#include "constants.hpp"
+
 int main() {
 
-	auto ssck = tcpConn::initializeServer(800, 4);
+	auto ssck = tcpConn::initializeServer(DEFAULT_PORT, 4);
 
-	while (true) {
-		auto client = tcpConn::acceptClientSock(ssck);
-
-		if (client != -1) {
-			const char* fy = "No, FuckYou Too";
-			tcpConn::sendSegmentC(client, fy);
-			break;
-		}
-	}
 }
