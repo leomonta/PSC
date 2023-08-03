9909748484f62abd9d080f629bb8a4fb0dc4cd4b
Leonard <remon.w.dev@gmail.com>
2023-08-03 11:20:10 +0200

    added a coule of string helpers

--- common/include/utils.hpp
+++ common/include/utils.hpp
@@ -2,16 +2,54 @@
 
 #include "header.hpp"
 
+#include <stddef.h>
+
 /**
  * Given a string at least TOT_HEADER_LEN long prints its bytes, 4 per line
- * 
+ *
  * @param head the header to print
  */
 void printHeaderStr(const char *head);
 
 /**
  * Given a Psc header struct pretty prints it
- * 
+ *
  * @param head the header to print
  */
 void printHeaderStruct(const PSCheader *head);
+
+/**
+ * given a 32bit int creates a string of the hex representation
+ *
+ * the string MUST be already allocated with a size of at least 8 bytes, one for each bit quartet
+ *
+ * @param val the value to conver
+ * @param strloc the string where to put the hex representation
+ * @param lowercase if true uses lowercase letters, if false uppercase letters
+ */
+void stringifyHex(const uint32_t val, char *strloc, const bool lowecase);
+
+/**
+ * find a string needle in the null terminated string haystack while making sure that not more than count characters are compared for haystack
+ *
+ * @param haystack where to search the other given string
+ * @param needle the null terminated string to search for
+ * @param count the maximum amount of character of haystack to check
+ *
+ * @return a pointer to the first character of the matches sequence or nullptr of count or no matches are found
+ */
+const char *strnstr(const char *haystack, const char *needle, const size_t count);
+
+/**
+ * Find the given string in the given file line by line
+ * if found set line and pos to the line number and column of the first occurrence
+ * 
+ * @param toSearch string to search in the file, must be null terminated
+ * @param file to seach into
+ * @param line line number of the found string
+ * @param col column in the line of the found string
+ * 
+ * @return true if the string was found in the file, false if not
+ * if return false the value of line and col shound be discarded
+ */
+bool findInFile(const char* toSearch, FILE *file, size_t *line, size_t *col);
--- common/src/utils.cpp
+++ common/src/utils.cpp
@@ -3,6 +3,7 @@
 #include "constants.hpp"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void printHeaderStr(const char *head) {
 
@@ -40,4 +41,78 @@ void printHeaderStruct(const PSCheader *head) {
 	printf("Timestamp at which is was sent -> %u\n", head->UUID);
 
 #endif
+}
+
+void stringifyHex(const uint32_t val, char *strloc, const bool lowercase) {
+
+	char a = 'A';
+	if (lowercase) {
+		a = 'a';
+	}
+
+	// 8 quartets 0x0000 in 32 bits
+	for (int i = 0; i < 8; ++i, strloc++) {
+		uint8_t quartet = val >> (8 - i - 1) * 4; // consiedare the correct quartet starting from the right-most
+		quartet &= 0b1111;                        // removes evetyhing other than the first 4 bits
+
+		if (quartet < 10) {
+			*strloc = '0' + quartet;
+		} else {
+			*strloc = a + quartet - 10;
+		}
+	}
+}
+
+const char *strnstr(const char *haystack, const char *needle, const size_t count) {
+
+	if (count == 0) {
+		return nullptr;
+	}
+
+	auto Itr = 0;
+
+	for (size_t i = 0; i < count; ++i) {
+
+		if (haystack[i] == needle[Itr]) {
+			++Itr;
+			if (needle[Itr] == '\0') {
+				// match
+				return haystack + i - Itr + 1;
+			}
+
+		} else {
+			// reset back the search
+			i -= Itr;
+			Itr = 0;
+		}
+	}
+
+	// no match
+	return nullptr;
+}
+
+
+bool findInFile(const char* toSearch, FILE *file, size_t *line, size_t *pos) {
+
+	bool userAlreadySaved = false;
+
+	size_t len  = 0;
+	char  *linestr = nullptr;
+	while (true) {
+		auto read = getline(&linestr, &len, file);
+		if (read == -1) {
+			break;
+		}
+
+		auto foundPos = strnstr(linestr, toSearch, read);
+		if (foundPos != nullptr) {
+			return true;
+		}
+	}
+
+	if (line) {
+		free(line);
+	}
+
+	return false;
 }
